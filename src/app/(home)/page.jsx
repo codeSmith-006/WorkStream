7ea214ef3d3b2f5a4756ce8772f927b4c8cc5cbe
Ryan Rehan <ryanrehan.pc@gmail.com>
2025-09-16 01:00:15 +0600

    update

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -3,18 +3,19 @@ import React from "react";
 import { ReadyToTransform } from "../Components/(home_page)/ReadyToTransform/ReadyToTransform";
 import { Hero } from "../Components/(home_page)/Hero/Hero";
 import UseCases from "../Components/(home_page)/UseCases/UseCases";
-// import Hero from "../Components/(home_page)/Hero/Hero";
-
+import NeedToSucceed from "../Components/NeedToSucceed/NeedToSucceed";
 
 // here we'll have the content of the home page
 export default function HomePage() {
   return (
     <div>
       {/* hero section */}
-      <div><Hero></Hero></div>
+      <div>
+        <Hero></Hero>
+      </div>
       {/* Everything you need to succeed */}
       <div>
-        <NeedToSucceed/>
+        <NeedToSucceed />
       </div>
       {/* Perfect for every team */}
       <div>
