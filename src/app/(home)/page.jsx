968fb376a6b8265acb11823d6c0ed02692c2cd4a
Rakib-76 <mrrakibhasan76@gmail.com>
2025-09-15 20:15:49 +0600

    added the hero need to succed and ready to taransform section

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -4,6 +4,7 @@ import { ReadyToTransform } from "../Components/(home_page)/ReadyToTransform/Rea
 import { Hero } from "../Components/(home_page)/Hero/Hero";
 import UseCases from "../Components/(home_page)/UseCases/UseCases";
 // import Hero from "../Components/(home_page)/Hero/Hero";
+import NeedToSucceed from "../Components/(home_page)/NeedToSucceed/NeedToSucceed";
 
 // here we'll have the content of the home page
 export default function HomePage() {
@@ -12,7 +13,9 @@ export default function HomePage() {
       {/* hero section */}
       <div><Hero></Hero></div>
       {/* Everything you need to succeed */}
-      <div></div>
+      <div>
+        <NeedToSucceed/>
+      </div>
       {/* Perfect for every team */}
       <div>
         <UseCases></UseCases>
