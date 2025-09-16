0a1a1975e14d28490ec6859139725ee829a6ffd2
Ashfaq Sarker Abid <abidsarker213@gmail.com>
2025-09-16 20:05:00 +0600

    pricing section add

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { ReadyToTransform } from "../Components/(home_page)/ReadyToTransform/ReadyToTransform";
 import { Hero } from "../Components/(home_page)/Hero/Hero";
+import { Pricing } from "../Components/(home_page)/Pricing/Pricing";
 // import Hero from "../Components/(home_page)/Hero/Hero";
 
 // here we'll have the content of the home page
@@ -16,6 +17,8 @@ export default function HomePage() {
       <div></div>
       {/* Loved by teams worldwide */}
       <div></div>
+      {/* pricing */}
+      <div><Pricing></Pricing></div>
       {/* Ready to transform the way your team works? */}
       <div>
         <ReadyToTransform></ReadyToTransform>
