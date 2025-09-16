38c1be9afe038d1171f59ca1dc2b0744b4821af5
Rakib-76 <mrrakibhasan76@gmail.com>
2025-09-17 01:25:27 +0600

    added the back to the top section

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -5,6 +5,8 @@ import { Hero } from "../Components/(home_page)/Hero/Hero";
 import UseCases from "../Components/(home_page)/UseCases/UseCases";
 // import Hero from "../Components/(home_page)/Hero/Hero";
 import NeedToSucceed from "../Components/(home_page)/NeedToSucceed/NeedToSucceed";
+import ScrollToTopButton from "../Components/(home_page)/ScrollBottomToTop/ScrollToTopButton";
+import FaqSection from "../Components/(home_page)/FAQ/FaqSection";
 
 // here we'll have the content of the home page
 export default function HomePage() {
@@ -14,7 +16,7 @@ export default function HomePage() {
       <div><Hero></Hero></div>
       {/* Everything you need to succeed */}
       <div>
-        <NeedToSucceed/>
+        <NeedToSucceed />
       </div>
       {/* Perfect for every team */}
       <div>
@@ -22,9 +24,18 @@ export default function HomePage() {
       </div>
       {/* Loved by teams worldwide */}
       <div></div>
+      {/* FAQ section */}
+      <div>
+        <FaqSection />
+      </div>
+
       {/* Ready to transform the way your team works? */}
       <div>
         <ReadyToTransform></ReadyToTransform>
+      </div>
+      {/* Back to top scroll component */}
+      <div>
+        <ScrollToTopButton />
       </div>
     </div>
   );
