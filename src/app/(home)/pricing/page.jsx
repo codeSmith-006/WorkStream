9dc54c5e6c96beb054aaf45259845a9fc3b07960
Ryan Rehan <ryanrehan.pc@gmail.com>
2025-09-17 18:41:22 +0600

    pricing route added

---
+++ src/app/(home)/pricing/page.jsx
@@ -0,0 +1,10 @@
+import { Pricing } from "../../Components/(home_page)/Pricing/Pricing";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="pt-24 md:pt-32">
+      <Pricing></Pricing>
+    </div>
+  );
+}
