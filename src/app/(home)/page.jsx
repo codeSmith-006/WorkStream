a132ca8a0633c50c381c1fe5776c5c58d5f7d00c
Rakib-76 <mrrakibhasan76@gmail.com>
2025-09-17 12:56:53 +0600

    add the scroll back to top

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -3,17 +3,16 @@ import React from "react";
 import { ReadyToTransform } from "../Components/(home_page)/ReadyToTransform/ReadyToTransform";
 import { Hero } from "../Components/(home_page)/Hero/Hero";
 import UseCases from "../Components/(home_page)/UseCases/UseCases";
-<<<<<<< HEAD
 // import Hero from "../Components/(home_page)/Hero/Hero";
 import NeedToSucceed from "../Components/(home_page)/NeedToSucceed/NeedToSucceed";
-import FAQ from "../Components/(home_page)/FAQ/FaqSection";
+// import FAQ from "../Components/(home_page)/FAQ/FaqSection";
 
-=======
-import NeedToSucceed from "../Components/NeedToSucceed/NeedToSucceed";
+// import NeedToSucceed from "../Components/NeedToSucceed/NeedToSucceed";
 import Contact from "../Components/(home_page)/Contact/Contact";
 import { Pricing } from "../Components/(home_page)/Pricing/Pricing";
 import { Testimonials } from "../Components/(home_page)/Testimonials/Testimonials";
->>>>>>> 017ef9e61504f88e40f89138849f4492eb66f2e7
+import FAQ from "../Components/(home_page)/FAQ/FAQSection";
+import ScrollToTopButton from "../Components/(home_page)/ScrollBackToTop/ScrollBackToTop";
 
 // here we'll have the content of the home page
 export default function HomePage() {
@@ -32,20 +31,17 @@ export default function HomePage() {
         <UseCases></UseCases>
       </div>
       {/* Loved by teams worldwide */}
-<<<<<<< HEAD
       <div></div>
       {/* FAQ section */}
       <div>
         <FAQ/>
       </div>
     
-=======
       <div>
         <Testimonials></Testimonials>
       </div>
       {/* pricing */}
       <div><Pricing></Pricing></div>
->>>>>>> 017ef9e61504f88e40f89138849f4492eb66f2e7
       {/* Ready to transform the way your team works? */}
       <div>
         <ReadyToTransform></ReadyToTransform>
@@ -54,6 +50,8 @@ export default function HomePage() {
       <div>
         <Contact></Contact>
       </div>
+      {/* Scroll back to top */}
+      <ScrollToTopButton/>
     </div>
   );
 }
